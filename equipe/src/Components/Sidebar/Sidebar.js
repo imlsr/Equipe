ea5cb755ccea32e0095f8f3f3fef97c9b5f2cca3
imlsr <likhith.sr@studentpartner.com>
2020-09-12 19:34:36 +0530

    Sidebar Options

--- equipe/src/Components/Sidebar/Sidebar.js
+++ equipe/src/Components/Sidebar/Sidebar.js
@@ -1,7 +1,18 @@
 import React from "react"
 import "./Sidebar.css"
+import SidebarOption from "./SidebarOption"
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
 import CreateIcon from "@material-ui/icons/Create"
+import InsertCommentIcon from "@material-ui/icons/InsertComment"
+import ExpandLessIcon from "@material-ui/icons/ExpandLess"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import AddIcon from "@material-ui/icons/Add"
+import InboxIcon from "@material-ui/icons/Inbox"
+import DraftsIcon from "@material-ui/icons/Drafts"
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder"
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt"
+import AppsIcon from "@material-ui/icons/Apps"
+import FileCopyIcon from "@material-ui/icons/FileCopy"
 
 function Sidebar() {
     return (
@@ -16,6 +27,19 @@ function Sidebar() {
                 </div>
                 <CreateIcon />
             </div>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+            <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & User Groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+            <hr />
+            <SidebarOption Icon={AddIcon} title="Add Channel" />
+            {/* Connect to db render all channels */}
         </div>
     )
 }
